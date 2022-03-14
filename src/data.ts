44a0d248d6acf77b52aab1a2618dd011690af477
Andras Varga <vargaae@hotmail.com>
2022-03-14 13:49:53 +0100

    start Board with Cards

--- src/data.ts
+++ src/data.ts
@@ -10,13 +10,30 @@ import card9 from './images/cards/ts.png';
 import card10 from './images/cards/webpack.png';
 import cardBack from './images/cards/white.png';
 
-export type CardInterface = {
+export const shuffleCards = (arr: any[]): any[] => {
+  return arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
+};
+
+export type CardType = {
   id: string;
-  flipped: boolean;
-  backImage: string;
-  frontImage: string;
+  turnedCard: boolean;
+  cardBackImage: string;
+  cardFrontImage: string;
   clickable: boolean;
   matchingCardId: string;
 };
 
 const cardsArray: string[] = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10];
+
+export const startCardsBoard = (): CardType[] =>
+  [...cardsArray, ...cardsArray].map((card, i) => ({
+    id: `card${i}`,
+    turnedCard: false,
+    cardBackImage: cardBack,
+    cardFrontImage: card,
+    clickable: true,
+    matchingCardId: i < cardsArray.length ? `card${i + cardsArray.length}` : `card${i - cardsArray.length}`
+  }));
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { startCardsBoard } from './../data';
 
 const Board = () => {
+  console.log(startCardsBoard())
   return <div className="board">GAMEBOARD</div>;
 };
 
