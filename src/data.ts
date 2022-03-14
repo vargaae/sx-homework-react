e06ed3071a6ce0755f09c7258be0359efffb4130
Andras Varga <vargaae@hotmail.com>
2022-03-14 13:05:20 +0100

    init data with card images

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+
+export const [] = [
+    {
+      name: 'angular',
+      img: 'images/cards/angular.png'
+    },
+    {
+      name: 'd3',
+      img: 'images/cards/d3.png'
+    },
+    {
+      name: 'jenkins',
+      img: 'images/cards/jenkins.png'
+    },
+    {
+      name: 'postcss',
+      img: 'images/cards/postcss.png'
+    },
+    {
+      name: 'react',
+      img: 'images/cards/react.png'
+    },
+    {
+      name: 'redux',
+      img: 'images/cards/redux.png'
+    },
+    {
+      name: 'sass',
+      img: 'images/cards/sass.png'
+    },
+    {
+      name: 'splendex',
+      img: 'images/cards/splendex.png'
+    },
+    {
+      name: 'ts',
+      img: 'images/cards/ts.png'
+    },
+    {
+      name: 'webpack',
+      img: 'images/cards/webpack.png'
+    }
+];
